02131348c1347f04189eb1a9da78d6a88e507368
Ahyaan Hassan <67073756+ahyaanhassan@users.noreply.github.com>
2024-11-16 14:11:31 +0530

    HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -105,7 +105,7 @@ export default function HeroSection() {
         <div className="mx-auto px-14 my-4 grid grid-cols-1 lg:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className='text-white mb-4 text-3xl md:text-4xl lg:text-6xl font-extrabold'>
-                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-600'>Hello, I'm {" "}</span> <br /> 
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-red-600'>Hello, I'm {" "}</span> <br /> 
                     <TypeAnimation
                         sequence={[
                             // Same substring at the start will only be typed out once, initially
@@ -128,13 +128,13 @@ export default function HeroSection() {
                 <div className="">
                     <Link
                         href="#contact"
-                        className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-blue-500 hover:bg-slate-200 text-white"
+                        className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-red-500 hover:bg-slate-200 text-white"
                     >
                         Hire Me
                     </Link>
                     <Link
-                        href="https://drive.google.com/file/d/1-NSgR-piyowAPihAlJPVeYQ657-4pOdI/view?usp=drive_link"
-                        className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-blue-500 hover:bg-slate-800 text-white mt-3"
+                        href="https://drive.google.com/file/d/1-NSgR-piyowAPihAlJPVeYQ657-4pOdI/view?usp=sharing"
+                        className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-red-500 hover:bg-slate-800 text-white mt-3"
                     >
                         <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                             Download CV
